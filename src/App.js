634cc026dc3731fa3df1bf687caaaf78b57cfa8f
Bowen Zhang <bz2306@nyu.edu>
2023-07-20 12:48:29 -0400

    First commit for setting up configurations and fundamental layout of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header
+        style={{
+          display: "flex",
+          justifyContent: "space_between",
+          backgroundColor: "#FB8C00",
+        }}
+      >
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+          Letgo: second-hand market
+        </div>
+      </Header>
+      <Content
+        style={{
+          height: "calc(100% - 64px)",
+          padding: 20,
+          overflow: "auto",
+          backgroundColor: "#E3F2FD",
+        }}
+      >
+        <div>This is Placeholder for the Letgo Market</div>
+      </Content>
+    </Layout>
   );
 }
 
