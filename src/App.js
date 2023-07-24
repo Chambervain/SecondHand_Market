1b8f2545311e6b66bc20bddb0edde620e78cbf47
Bowen Zhang <bz2306@nyu.edu>
2023-07-23 23:01:00 -0400

    Built the fundamental architecture and function paging of Tab component for the project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
+import { useState } from "react";
 import "./App.css";
-import { Layout } from "antd";
+import { Button, Dropdown, Layout, Space } from "antd";
+import { Tabs } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 const { Header, Content } = Layout;
 
 function App() {
+  // Here, Set authed state to be true in order to display Tab component, cuz there is no async function so far
+  const [authed, setAuthed] = useState(true);
+
+  const renderContent = () => {
+    if (authed) {
+      return (
+        <Tabs defaultActiveKey="1" centered>
+          <Tabs.TabPane tab="Home Page" key="1">
+            Content of Tab Pane 1
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="My Items" key="2">
+            Content of Tab Pane 2
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Upload Item" key="3">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+        </Tabs>
+      );
+    }
+
+    return <div>This is Placeholder of homepage for the Letgo Market</div>;
+  };
+
   return (
     <Layout style={{ height: "100vh" }}>
       <Header
         style={{
           display: "flex",
-          justifyContent: "space_between",
+          justifyContent: "space-between",
           backgroundColor: "#FB8C00",
         }}
       >
         <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
           Letgo: second-hand market
         </div>
+
+        {authed && (
+          <div>
+            <Space>
+              <Button type="primary" shape="round">
+                Sign Up
+              </Button>
+
+              <Button type="primary" shape="round">
+                Sign In
+              </Button>
+            </Space>
+          </div>
+        )}
       </Header>
       <Content
         style={{
@@ -25,7 +65,7 @@ function App() {
           backgroundColor: "#E3F2FD",
         }}
       >
-        <div>This is Placeholder for the Letgo Market</div>
+        {renderContent()}
       </Content>
     </Layout>
   );
