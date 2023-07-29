9433e05a31ca90c5fbd7b347b5a5f609fd13c617
Bowen Zhang <bz2306@nyu.edu>
2023-07-29 11:56:52 -0400

    Add functionalities of search by keyword and search by category to present items card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,60 @@
 import { useEffect, useState } from "react";
-import { getAllItems } from "../utils";
-import { Card, Carousel, List, message, Image } from "antd";
+import {
+  getAllItems,
+  getItemsByCategory,
+  searchItemsByKeyword,
+} from "../utils";
+import {
+  Card,
+  Carousel,
+  List,
+  message,
+  Image,
+  Input,
+  Space,
+  Button,
+  Dropdown,
+} from "antd";
 import Text from "antd/lib/typography/Text";
-import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
+import {
+  DownOutlined,
+  LeftCircleOutlined,
+  RightCircleOutlined,
+  SkinOutlined,
+  LaptopOutlined,
+  SketchOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
+
+const { Search } = Input;
+
+const items = [
+  {
+    label: "Clothing",
+    key: "Clothing",
+    icon: <SkinOutlined />,
+  },
+  {
+    label: "Electronics",
+    key: "Electronics",
+    icon: <LaptopOutlined />,
+  },
+  {
+    label: "Jewelry & Watches",
+    key: "Jewelry & Watches",
+    icon: <SketchOutlined />,
+  },
+  {
+    label: "Household goods",
+    key: "Household goods",
+    icon: <HomeOutlined />,
+  },
+];
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -22,8 +70,74 @@ const Home = () => {
       });
   }, []);
 
+  const onSearch = async (value) => {
+    setKeyword(value);
+
+    if (keyword === "") {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const data = await searchItemsByKeyword(keyword);
+      setData(data);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleMenuClick = async (e) => {
+    // message.info("Click on menu item.");
+    // console.log("parameter", e.key);
+
+    setLoading(true);
+
+    try {
+      const data = await getItemsByCategory(e.key);
+      setData(data);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+
   return (
     <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Search
+          style={{ position: "relative", width: "24%" }}
+          loading={loading}
+          placeholder="Input your search keyword"
+          onSearch={onSearch}
+          enterButton
+        />
+        <Dropdown menu={menuProps}>
+          <Button shape="circle-outline">
+            <Space>
+              Choose your product category
+              <DownOutlined />
+            </Space>
+          </Button>
+        </Dropdown>
+      </div>
+      <br />
+      <br />
+
       <List
         style={{ marginTop: 20 }}
         loading={loading}
@@ -44,14 +158,16 @@ const Home = () => {
               title={
                 <div
                   style={{
-                    // Here, comment out the line of flex display to ensure the product title is in center of card
-                    // display: "flex",
+                    display: "flex",
                     alignItems: "center",
-                    textAlign: "center",
+                    justifyContent: "space-between",
                   }}
                 >
                   <Text ellipsis={true} style={{ maxWidth: 150 }}>
                     {item.item_name}
+                  </Text>
+                  <Text ellipsis={true} style={{ maxWidth: 150 }}>
+                    ${item.item_price}
                   </Text>
                 </div>
               }
